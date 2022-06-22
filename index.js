08d7db40841ac3e393242c70fa517a8107469218
Iñigo Taibo <itaibo@hey.com>
2022-06-22 21:00:43 +0200

    Export as module

--- index.js
+++ index.js
@@ -10,22 +10,23 @@ const chainHash = '8990e7a9aaed2ffed73dbd7092123d6f289930540d7651336225dc172e51b
 const urls = ['https://api.drand.sh'];
 const options = { chainHash };
 
-// Start client
-const client = await Client.wrap(HTTP.forURLs(urls, chainHash), options);
+export default async function generateRandomNumber() {
+	// Start client
+	const client = await Client.wrap(HTTP.forURLs(urls, chainHash), options);
 
-// Get current round
-const currentRound = client.roundAt(Date.now());
+	// Get current round
+	const currentRound = client.roundAt(Date.now());
 
-// Get randomness for current round
-const result = await client.get(currentRound);
+	// Get randomness for current round
+	const result = await client.get(currentRound);
 
-// Extract randomness
-const randomness = result?.randomness;
-if (!randomness) throw new Error('Could not get randomness');
+	// Extract randomness
+	const randomness = result?.randomness;
+	if (!randomness) throw new Error('Could not get randomness');
 
-// Generate random number with extracted randomness
-const prng = seedrandom(randomness, { entropy: true });
-const randomNumber = prng();
+	// Generate random number with extracted randomness
+	const prng = seedrandom(randomness, { entropy: true });
+	const randomNumber = prng();
 
-console.log(randomNumber);
-process.exit();
+	return randomNumber;
+}
